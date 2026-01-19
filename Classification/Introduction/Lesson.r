a24b4b18be071cd50ad5c1045687b72162998e3a
Jesper Sandtangen <j.sandtangen@outlook.com>
2026-01-19 14:22:51 +0100

    Wokring on R

--- Classification/Introduction/Lesson.r
+++ Classification/Introduction/Lesson.r
@@ -1,5 +1,22 @@
-install.packages("tidyverse")
-install.packages("tidymodels")
-install.packages("nycflights13")
-library(nycflights13)
-install.packages("themis")
+install.packages(c("tidyverse", "tidymodels", "DataExplorer", "here"),
+                 lib = "C:/Users/jsand/AppData/Local/R/win-library")
+
+.libPaths("C:/Users/jsand/AppData/Local/R/win-library")
+
+library(tidyverse)
+library(tidymodels)
+library(DataExplorer)
+
+df = read_csv(file = "https://raw.githubusercontent.com/microsoft/ML-For-Beginners/main/4-Classification/data/cuisines.csv")
+
+# View the first 5 rows
+df %>% 
+  slice_head(n = 5)
+
+# Basic information about the data
+df %>%
+  introduce()
+
+# Visualize basic information above
+df %>% 
+  plot_intro(ggtheme = theme_light())
